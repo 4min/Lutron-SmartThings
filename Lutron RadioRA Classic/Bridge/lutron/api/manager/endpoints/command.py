26c1f12bc6d44da8ef5ce3ab248fc439fb788910
Ilya Fomin <fomin@google.com>
2020-06-21 22:11:36 -0700

    Fixed the command api endpoint

--- Lutron RadioRA Classic/Bridge/lutron/api/manager/endpoints/command.py
+++ Lutron RadioRA Classic/Bridge/lutron/api/manager/endpoints/command.py
@@ -3,6 +3,9 @@ import logging
 
 from flask import request
 from flask_restplus import Resource
+
+from flask import current_app as app
+
 from lutron.api.manager.dbmethods import create_zonetype, delete_zonetype, update_zonetype
 from lutron.api.restplus import api
 from lutron.database.models import Zone, Zonetype
@@ -13,24 +16,22 @@ log = logging.getLogger(__name__)
 # require a config option to enable this for those who don't care)
 ns = api.namespace('command', description='Raw RadioRA Classic command operations (*DEPRECATED*)')
 
-raSerial = None
-
 LUTRON = 'lutron'
 
 @ns.route('/')
 class ZMPI(Resource):
     def get(self):
-        raSerial.writeCommand("ZMPI")
-        return { LUTRON: raSerial.readData() }
+        app.raSerial.writeCommand("ZMPI")
+        return { LUTRON: app.raSerial.readData() }
     
 @ns.route('/<cmd>')
 class ApiLutronCmd(Resource):
     def get(self, cmd):
-        raSerial.writeCommand(cmd)
-        return { LUTRON: raSerial.readData() }
+        app.raSerial.writeCommand(cmd)
+        return { LUTRON: app.raSerial.readData() }
 
 @ns.route('/<cmd>/zone/<zone>/level/<level>')
 class ApiLutronMultiCmd(Resource):
     def get(self, cmd, zone, level):
-        raSerial.writeCommand(cmd + "," + zone + "," + level)
-        return { LUTRON: raSerial.readData() }
+        app.raSerial.writeCommand(cmd + "," + zone + "," + level)
+        return { LUTRON: app.raSerial.readData() }
